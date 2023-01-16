ca0cc78c8c4b9a02b6d54140c3c4072f198dad8c
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-01-15 20:56:07 -0800

    added/fixed timer function and score funtion

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var wrapper = document.querySelector(".wrapper");
 var highscore = [];
 var initials = [];
-var currentScore = 0;
 var state = 0;
-var questions = {
+var timer = 75;
+var timeInterval;
+const questions = {
     q1: {
         question:"Commonly used data types DO NOT Include:",
         a1:"1. strings",
@@ -41,13 +41,20 @@ var questions = {
     }
 };
 
+var timerEl = document.getElementById("timer");
+//var El = document.getElementById("");
+
+// Activates when view high score is clicked on top left of screen.
 function viewHighScore(){
-    currentScore = 0;
+    // Reset state of the page
     state = 0;
-    document.getElementById("timer").textContent = 0;
+    timer = 75;
+    stopTimer();
+    timerEl.textContent = "Timer: " + timer;
     document.getElementById("navbar").hidden = true;
     document.getElementById("mainText").textContent = "High Scores";
 
+    // Hides all other pages just in case
     document.getElementById("startText").hidden = true;
     document.getElementById("startButton").hidden = true;
     document.getElementById("a1").hidden = true;
@@ -60,7 +67,7 @@ function viewHighScore(){
     document.getElementById("initials").hidden = true;
     document.getElementById("initialsSubmit").hidden = true;
 
-
+    // Update scoreList display and show score page
     for(var i = 0; i < highscore.length; i++){
         document.getElementById("scoreList").textContent += initials[i]+": "+highscore[i] + ", ";
     }
@@ -69,12 +76,7 @@ function viewHighScore(){
     document.getElementById("clearHighScore").hidden = false;
 }
 
-function setTime(x,y,z){
-    var timeInterval = setInterval(runTime(), 1000);
-    console.log(x);
-
-}
-
+// Activates when Start quiz is clicked. Displays first question.
 function startQuiz(){
     // Hide start panel
     document.getElementById("startText").hidden = true;
@@ -92,6 +94,49 @@ function startQuiz(){
     document.getElementById("a3").hidden = false;
     document.getElementById("a4").hidden = false;
     state = 1;
+
+    //Start timer 
+    startTimer();
+}
+
+// Starts the timer when the quiz begins
+function startTimer(){
+    timeInterval = setInterval(function() {
+        // If timer is 0 or negative, stop the timer
+        if (timer <= 0){
+            stopTimer();
+        
+        // If timer is positive, update timer
+        }else{
+            timer--;
+            document.getElementById("timer").textContent = "Timer: " + timer;
+        }
+    }, 1000);
+}
+
+// Stops the timer, hides questions, displays results page.
+function stopTimer(){
+    // Stops the timer function
+    clearInterval(timeInterval);
+
+    // Hide the question pages
+    document.getElementById("a1").hidden = true;
+    document.getElementById("a2").hidden = true;
+    document.getElementById("a3").hidden = true;
+    document.getElementById("a4").hidden = true;
+    document.getElementById("result").hidden = true;
+
+    // Display the result page
+    document.getElementById("mainText").textContent = "All Done!";
+    document.getElementById("initialsText1").textContent = "Your final score is " + timer + ".";
+    document.getElementById("initials").textContent = "";
+
+    document.getElementById("initialsText1").hidden = false;
+    document.getElementById("initialsText2").hidden = false;
+    document.getElementById("initials").value = "";
+    document.getElementById("initials").hidden = false;
+    document.getElementById("initialsSubmit").hidden = false;
+    state= 0;
 }
 
 function answerQuestion1(){
@@ -111,18 +156,24 @@ function answerQuestion4(){
 }
 
 function answerQuestion(answer){
+    // Handles question 1 results and displays question 2
     if(state == 1){
         document.getElementById("mainText").textContent = questions.q2.question;
         document.getElementById("a1").textContent = questions.q2.a1;
         document.getElementById("a2").textContent = questions.q2.a2;
         document.getElementById("a3").textContent = questions.q2.a3;
         document.getElementById("a4").textContent = questions.q2.a4;
+        document.getElementById("result").hidden = false;
         if(answer == 3){
-            currentScore++;
+            document.getElementById("result").textContent = "Correct!";
         }else{
-
+            document.getElementById("result").textContent = "Wrong!";
+            timer-=10;
+            document.getElementById("timer").textContent = "Timer: " + timer;
         }
         state++;
+
+    // Handles question 2 results and displays question 3
     }else if(state == 2){
         document.getElementById("mainText").textContent = questions.q3.question;
         document.getElementById("a1").textContent = questions.q3.a1;
@@ -130,11 +181,15 @@ function answerQuestion(answer){
         document.getElementById("a3").textContent = questions.q3.a3;
         document.getElementById("a4").textContent = questions.q3.a4;
         if(answer == 3){
-            currentScore++;
+            document.getElementById("result").textContent = "Correct!";
         }else{
-
+            document.getElementById("result").textContent = "Wrong!";
+            timer-=10;
+            document.getElementById("timer").textContent = "Timer: " + timer;
         }
         state++;
+
+    // Handles question 3 results and displays question 4
     }else if(state == 3){
         document.getElementById("mainText").textContent = questions.q4.question;
         document.getElementById("a1").textContent = questions.q4.a1;
@@ -142,11 +197,15 @@ function answerQuestion(answer){
         document.getElementById("a3").textContent = questions.q4.a3;
         document.getElementById("a4").textContent = questions.q4.a4;
         if(answer == 4){
-            currentScore++;
+            document.getElementById("result").textContent = "Correct!";
         }else{
-
+            document.getElementById("result").textContent = "Wrong!";
+            timer-=10;
+            document.getElementById("timer").textContent = "Timer: " + timer;
         }
         state++;
+
+    // Handles question 4 results and displays question 5
     }else if(state == 4){
         document.getElementById("mainText").textContent = questions.q5.question;
         document.getElementById("a1").textContent = questions.q5.a1;
@@ -154,49 +213,45 @@ function answerQuestion(answer){
         document.getElementById("a3").textContent = questions.q5.a3;
         document.getElementById("a4").textContent = questions.q5.a4;
         if(answer == 3){
-            currentScore++;
+            document.getElementById("result").textContent = "Correct!";
         }else{
-
+            document.getElementById("result").textContent = "Wrong!";
+            timer-=10;
+            document.getElementById("timer").textContent = "Timer: " + timer;
         }
         state++;
+
+    // Handles question 5 results, stops timer, and displays results
     }else if(state == 5){
         if(answer == 4){
-            currentScore++;
+            document.getElementById("result").textContent = "Correct!";
         }else{
-
+            document.getElementById("result").textContent = "Wrong!";
+            timer-=10; //timer = timer - 10;
+            document.getElementById("timer").textContent = "Timer: " + timer;
         }
-        document.getElementById("a1").hidden = true;
-        document.getElementById("a2").hidden = true;
-        document.getElementById("a3").hidden = true;
-        document.getElementById("a4").hidden = true;
-
-        document.getElementById("mainText").textContent = "All Done!";
-        document.getElementById("initialsText1").textContent = "Your final score is " + currentScore + ".";
-        document.getElementById("initials").textContent = "";
-
-        document.getElementById("initialsText1").hidden = false;
-        document.getElementById("initialsText2").hidden = false;
-        document.getElementById("initials").value = "";
-        document.getElementById("initials").hidden = false;
-        document.getElementById("initialsSubmit").hidden = false;
-        state= 0;
+        stopTimer();
     }
 }
 
+// Activates when initials are entered after the quiz
 function enterInitials(){
+    // Ensures initials are not empty
     if(document.getElementById("initials").value.length > 0){
+        //Updates the initials list and highscore list
         initials.push(document.getElementById("initials").value);
-        highscore.push(currentScore);
-        currentScore = 0;
+        highscore.push(timer);
+        timer = 75;
 
+        // Hides the results page
         document.getElementById("navbar").hidden = true;
-        document.getElementById("mainText").textContent = "High Scores";
         document.getElementById("initialsText1").hidden = true;
         document.getElementById("initialsText2").hidden = true;
         document.getElementById("initials").hidden = true;
         document.getElementById("initialsSubmit").hidden = true;
 
-
+        // Displays the high score page
+        document.getElementById("mainText").textContent = "High Scores";
         for(var i = 0; i < highscore.length; i++){
             document.getElementById("scoreList").textContent += initials[i]+": "+highscore[i] + ", ";
         }
@@ -206,14 +261,18 @@ function enterInitials(){
     }
 }
 
+// Empties the highscore list, initials list, and displays the start quiz page
 function clearHighScores(){
     highscore = [];
     initials = [];
     goBack();
 }
 
+// Displays the start quiz page
 function goBack(){
     document.getElementById("navbar").hidden = false;
+    timer = 75;
+    document.getElementById("timer").textContent = "Timer: " + timer;
     document.getElementById("scoreList").textContent = "";
     document.getElementById("scoreList").hidden = true;
     document.getElementById("goBack").hidden = true;
@@ -222,6 +281,8 @@ function goBack(){
     document.getElementById("startText").hidden = false;
     document.getElementById("startButton").hidden = false;
 }
+
+// Adds eventListeners into buttons
 var viewHighScoreButton = document.getElementById("viewHighScore");
 var startQuizButton = document.getElementById("startButton");
 var a1Button = document.getElementById("a1");
